6c9d8b9253304d3d4fdb1aa8d60a59ced7ecd232
ckirua <gregorferrer@gmail.com>
2025-04-02 23:44:49 +0000

    update stub and version

--- ctime/ranges.pyi
+++ ctime/ranges.pyi
@@ -1,5 +1,10 @@
 from datetime import datetime, timedelta
 
-def date_range_cython(
+import numpy as np
+
+def datetime_range(
     start: datetime, end: datetime, step: timedelta
 ) -> list[datetime]: ...
+def timestamp_s_range(
+    start: datetime, end: datetime, step: timedelta
+) -> np.ndarray[np.int64]: ...
